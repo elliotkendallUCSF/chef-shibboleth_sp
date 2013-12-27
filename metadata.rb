1138a726d1e82821322661eceb61a47174cbacec
elliotkendallUCSF <elliot.kendall@ucsf.edu>
2013-12-27 11:37:22 -0800

    Switch to ruby metadata

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+maintainer        "Elliot Kendall"
+maintainer_email  "elliot.kendall@ucsf.edu"
+license           "Apache 2.0"
+description       "Install and configure Shibboleth SP"
+version           "0.1.1"
+
+recipe "shibboleth_sp", "Install and configure Shibboleth SP"
+
+%w{ centos }.each do |os|
+  supports os
+end
